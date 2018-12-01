f1fd4f4920b59c5f1fe768df9baed640e5f371e4
Ilja Dubinin <iliadubinin91@gmail.com>
2018-12-01 12:15:39 +0000

    add do on each line function

---
+++ FileHelper.go
@@ -0,0 +1,20 @@
+package fileHelper
+
+
+import (
+	"os"
+	"bufio"
+)
+
+func DoOnEachLine(pathToFile string, doer func(line string)) {
+	fileHandler, err := os.Open(pathToFile)
+	if err != nil {
+		panic(err)
+	}
+	defer fileHandler.Close()
+
+	scanner := bufio.NewScanner(fileHandler)
+	for scanner.Scan() {
+		doer(scanner.Text()) 
+	}
+}
